6ae7d16fda7aaca93d1359912b2511c775949602
dylan cairns <invision33@gmail.com>
2016-05-30 18:22:17 -0500

    connect new post form component  to firebase db

--- app/components/create-new-post/component.js
+++ app/components/create-new-post/component.js
@@ -2,8 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     actions: {
-        createPost: (newPost) => {
-            console.log(`${newPost.title}, ${newPost.author}`); 
-        }
+        createPost: function(model) {
+        this.sendAction('createPost', model);  
+                
+        // clear input fields 
+        this.set('newPost.title', null);
+        this.set('newPost.author', null);
+        this.set('newPost.text', null);
     }
+   }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model: () => {
+    model: function() {
         return {}; 
+    },
+    
+    actions: {
+        createPost: function(model) {
+            let post = this.store.createRecord('post', {
+                title: model.title,
+                text: model.text,
+                author: model.author,
+                createdDate: new Date()              
+            });
+            post.save()
+                .then(s => {
+                    console.log('success,' + s); 
+                })
+                .catch(e => {
+                    console.log(e.errors);
+                });; 
+        }
     }
 });
